00c9dff95b106ce131d7b1f48827c1c732358829
Chris Ferber <chris@ferbers.us>
2019-10-23 00:25:07 -0500

    complete assignment base requirements

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+       <div className="About">
+           <h2>About</h2>
+           <p>We are actors working with actors and we react to things.</p>
+       </div>
+    );
+  }
+}
+
+export default About;
